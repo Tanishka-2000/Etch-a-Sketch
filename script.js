afa9f7b9f628087dc53f6667b8dddb428c5be065
Tanishka Talwar <tanishkatalwar18@gmail.com>
2022-05-01 19:18:39 +0530

    make a 16 by 16 grid inside container div using display grid

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+let gridSize = 16;
+let container = document.querySelector(".container");
+for (let i = 0; i < (gridSize*gridSize); i++) {
+    let div = document.createElement("div");
+    container.appendChild(div);
+}
