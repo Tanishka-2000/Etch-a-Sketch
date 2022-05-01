5821e167c0ad85a29ff8c30cae53065734ba3ce3
Tanishka Talwar <tanishkatalwar18@gmail.com>
2022-05-01 19:23:51 +0530

    add eventListener to divs to etch on a 16 by 16 grid only

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ for (let i = 0; i < (gridSize*gridSize); i++) {
     let div = document.createElement("div");
     container.appendChild(div);
 }
+let divs = document.querySelectorAll(".container div");
+divs.forEach((div) => {
+    div.addEventListener("mouseenter",etched);
+});
+function etched(e){
+    e.target.className = "etched";
+}
