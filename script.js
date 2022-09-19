61f3594537b3156f59fb164580f12b792e4871b2
Shashank Talwar <shashanktalwar81@gmail.com>
2022-09-19 19:53:35 +0530

    start etching on mouse down

--- script.js
+++ script.js
@@ -1,37 +1,49 @@
 let gridSize = 16;
 let container = document.querySelector(".container");
 let button = document.querySelector(".gridChanger");
+let etch = false;
 
 // add divs to container dynamically and change their bgcolor on hover
 function makeGrid(){
     container.innerHTML = "";
     for (let i = 0; i < (gridSize*gridSize); i++) {
         let div = document.createElement("div");
+        div.addEventListener("mouseenter",etched);
+        div.addEventListener("mousedown",startEtch);
+        div.addEventListener("mouseup",stopEtch);
         container.appendChild(div);
     }
-    let divs = document.querySelectorAll(".container div");
-    divs.forEach((div) => {
-        div.addEventListener("mouseenter",etched);
-    });
+    // let divs = document.querySelectorAll(".container div");
+    // divs.forEach((div) => {
+    //     div.addEventListener("mouseenter",etched);
+    // });
 }
-
+// container.addEventListener('dblClick',function(){
+//     console.log('double clicked');
+//     classToAdd = classToAdd === 'etched' ? 'etched' : '';
+// });
 //function to change bgcolor by adding class
+
+const startEtch = () => etch = true;
+
+const stopEtch = () => etch = false;
+
 function etched(e){
-    e.target.className = "etched";
+    e.target.className = etch ? 'etched' : '';
 }
 button.addEventListener("click",changeGridsize);
 
 //function to change grid size based on user input
 function changeGridsize(){
     let size = prompt("Enter new size for grid");
-    console.log(size);
+    // console.log(size);
     if(isNaN(size) || size > 100) return;
     gridSize = size;
 
     // update grid template based on user input
     container.style.gridTemplateColumns = `repeat(${gridSize},1fr)`;
     container.style.gridTemplateRows = `repeat(${gridSize},1fr)`;
-    
+
     makeGrid();
 }
 
